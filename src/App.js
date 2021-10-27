b26223d5d59612d98285f86c73d65af090062772
lauraAlabau <arlaura3@gmail.com>
2021-10-27 11:31:46 +0200

    Form to create new users

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import useFormulario from "./hooks/useFormulario";
+import Input from "./components/Input.js";
+import Card from "./components/Card.js";
+import Container from "./components/Container.js";
+import Button from "./components/Button.js";
 
-function App() {
+const App = () => {
+  const [usuarios, setUsuarios] = useState([]);
+  const [formulario, handlechange, reset] = useFormulario({
+    name: "",
+    lastname: "",
+    email: "",
+  });
+
+  const submit = (e) => {
+    e.preventDefault();
+    setUsuarios([...usuarios, formulario]);
+    reset();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{marginTop:'15%'}}>
+      <Container>
+        <Card>
+          <div style={{ padding: 20 }}>
+            <form onSubmit={submit}>
+              <Input
+                label="Nombre"
+                name="name"
+                value={formulario.name}
+                onChange={handlechange}
+                placeholder='Nombre'
+              />
+              <Input
+                label="Apellido"
+                name="lastname"
+                value={formulario.lastname}
+                onChange={handlechange}
+                placeholder='Apellido'
+              />
+              <Input
+                label="Correo"
+                name="email"
+                value={formulario.email}
+                onChange={handlechange}
+                placeholder='Correo'
+              />
+              <Button>Enviar</Button>
+            </form>
+          </div>
+        </Card>
+        <Card>
+          <ul>
+            {usuarios.map((usuario) => (
+              <li
+                key={usuario.email}
+              >{`${usuario.name} ${usuario.lastname}: ${usuario.email}`}</li>
+            ))}
+          </ul>
+        </Card>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import './Card.css'
+
+import React from 'react'
+
+const Card = ({children}) => {
+    return (
+        <div className="card">
+            {children}
+        </div>
+    )
+}
+
+export default Card
